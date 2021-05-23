7abd4dcc7be4b8b3598d61a1bb6775c01411ed6a
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-05-23 23:22:08 +0530

    decimal to binary

--- decimal to binary.py
+++ decimal to binary.py
@@ -1,10 +1,10 @@
 a=int(input('enter the number: '))
 b=[]
-while(a!=0):
+while(a!=1):
     if(a%2==0):
         b.append(0)
     else:
         b.append(1)
-    a=a/2
+    a=a//2
 print(b)            
     
