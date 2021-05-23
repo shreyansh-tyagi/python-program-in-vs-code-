1ce8d9f78870dc44d9b18bfa31cb2dfc8081742c
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-05-23 23:43:49 +0530

    decimal to binary

--- binary to decimal.py
+++ binary to decimal.py
@@ -2,4 +2,7 @@ a=input('entre the number: ')
 b=[]
 for i in range(len(a)):
     b.append(int(a[i]))
-print(b)    
+sum=0    
+for i in reversed(range(len(b))) :
+      sum+=pow(2,i)*b[i]
+print(sum)      
