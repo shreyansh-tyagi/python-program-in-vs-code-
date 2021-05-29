5000b58ae7cfcd08a2f8801d493c688c29029176
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-05-29 22:51:37 +0530

    combination of set of number

--- combination of set of number.py
+++ combination of set of number.py
@@ -1,6 +1,6 @@
 from itertools import product
 a,b=map(int,input('enter two numbers: ').split())
-print("The cartesian product using repeat:") 
+print("The cartesian product of two number using repeat:") 
 d=(list(product([a,b], repeat = a))) 
 print(d)
 c=(len(d))
